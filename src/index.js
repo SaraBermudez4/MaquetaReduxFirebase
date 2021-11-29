f90b9bfd99d77ce59a6d804d89c45e6e1c3e7842
Sara Bermudez <bermudezalvarezsaraestefania@gmail.com>
2021-11-29 15:54:16 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/style.css';
+import { RegistroApp } from './RegistroApp';
+
+ReactDOM.render(
+  <RegistroApp />,
+  document.getElementById('root')
+);
+
